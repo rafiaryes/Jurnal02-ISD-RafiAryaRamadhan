ce126378a9896645954d03959e42d0915e6589d4
rafiaryes <145308280+rafiaryes@users.noreply.github.com>
2024-03-21 10:48:12 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,32 +1,51 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        GenArrayList<ATK> daftarATK = new GenArrayList<>(5);
-        GenArrayList<Bag> daftarBag = new GenArrayList<>(5);
+        Scanner input = new Scanner(System.in);
+        Queue<String> queue = new LinkedList<>();
+        while (true) {
+            System.out.println("1. Input Tugas");
+            System.out.println("2. Tugas Yang Harus Dikerjakan Terlebih Dahulu");
+            System.out.println("3. Hapus Tugas");
+            System.out.println("4. Tampilkan Tugas");
+            System.out.println("5. Exit");
 
-        daftarATK.addData(new ATK("A001", "Pulpen Kenko", "Pulpen", 100));
-        daftarATK.addData(new ATK("A002", "Pensil Stabilo 2B", "Pensil", 100));
-        daftarATK.addData(new ATK("A003", "Spidol Snowman", "Spidol", 100));
-        daftarATK.addData(new ATK("B001", "Sinar Dunia", "Buku Tulis", 100));
-        daftarATK.addData(new ATK("B002", "Paper One", "Kertas HVS", 100));
-
-        daftarBag.addData(new Bag("C001", "Tas Eiger", "Tas", 100));
-        daftarBag.addData(new Bag("C002", "Tempat Pensil Faber Castle", "Tempat Pensil", 100));
-        daftarBag.addData(new Bag("C003", "Papan Jalan Faber Castle", "Papan Jalan", 100));
-        daftarBag.addData(new Bag("C004", "Papan Tulis Whiteboard", "Papan Tulis", 100));
-        daftarBag.addData(new Bag("C001", "Streples Joyko", "Streples", 100));
-
-        daftarATK.display();
-        daftarBag.display();
-
-        System.out.println();
-        System.out.println("DAFTAR BARANG SETELAH DIREMOVE");
-        
-        daftarATK.removeData(0);
-        daftarATK.display();
-
-        daftarBag.removeData(0);
-        daftarBag.display();
+            String pilih = input.nextLine();
+            switch (pilih) {
+                case "1":
+                    System.out.println("Tambahkan Tugas: ");
+                    String task = input.nextLine();
+                    queue.add(task);
+                    break;
+                case "2":
+                    if (!queue.isEmpty()) {
+                        System.out.println("Tugas yang harus dikerjakan " + queue.peek());
+                    } else {
+                        System.out.println("Tidak ada tugas yang harus dikerjakan");
+                    }
+                    break;
+                case "3":
+                    if (!queue.isEmpty()) {
+                        System.out.println("Tugas yang sudah selesai adalah " + queue.remove());
+                    } else {
+                        System.out.println("Tidak ada tugas yang harus dkerjakan");
+                    }
+                    break;
+                case "4":
+                    for (String tugas : queue) {
+                        System.out.println(tugas);
+                    }
+                    break;
+                case "5":
+                    input.close();
+                    System.out.println("Program selesai");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Pilih angka lain");
+                    break;
+            }
+        }
     }
 }
